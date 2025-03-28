6e6e12436ca63675f467c779f9cb60c4ba15d3af
S-SoHyun <slj506@naver.com>
2025-03-28 19:24:49 +0900

    Feat: UI 간 전환 기능 구현

---
+++ Assets/Scripts/UI/TitleUI.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleUI : BaseUI
+{
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button quitButton;
+
+    public override void Init(UIManager uiManager)
+    {
+        base.Init(uiManager);
+        playButton.onClick.AddListener(OnClickPlayButton);
+        quitButton.onClick.AddListener(OnClickQuitButton);
+    }
+
+    public void OnClickPlayButton()
+    {
+        UIManager.Instance.ChangeState(UIState.STAGE);
+    }
+
+    public void OnClickQuitButton()
+    {
+        Application.Quit();
+    }
+
+    protected override UIState GetUIState()
+    {
+        return UIState.TITLE;
+    }
+}
